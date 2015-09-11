5b3518a32a24ca51b389a1cb004428a65eaa8c40
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-09-11 21:25:50 +0900

    paradiso iOSプロジェクトを追加

---
+++ paradiso_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  paradiso_iOS
+//
+//  Created by 清 貴幸 on 2015/09/11.
+//  Copyright © 2015年 Team Paradiso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
