b6982bdf6c31bda19ee70842002ecb8087c5d222
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-09-12 10:05:41 +0900

    ダウンロード処理を関数に切り出し

--- paradiso_iOS/ViewController.swift
+++ paradiso_iOS/ViewController.swift
@@ -7,29 +7,38 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        let URL = NSURL(string: "http://videos2.cdn.xvideos.com/videos/flv/b/5/7/xvideos.com_b572536347e9f90306e4afa89965d101.flv?e=1441995373&ri=1024&rs=85&h=0168c9a9a556723080558040412e1da9")
-        if let URL = URL {
-            VideoDownloadService.downloadWithURL(URL, destination: { (temporaryURL, response) -> NSURL in
-                let fileManager = NSFileManager.defaultManager()
-                let directoryURLs = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-                if directoryURLs.count == 0 {
-                    return temporaryURL
-                }
-                
-                let pathComponent = response.suggestedFilename
-                return directoryURLs[0].URLByAppendingPathComponent(pathComponent!)
-            }).progress { bytesRead, totalBytesRead, totalBytesExpectedToRead in
-                print("byte:\(bytesRead) total: \(totalBytesRead) totalExpect: \(totalBytesExpectedToRead)")
-            }.response { request, response, _, error in
-                print(response)
+    }
+    
+    func downloadVideoWithURL(URL: NSURL) {
+        VideoDownloadService.downloadWithURL(URL, destination: { (temporaryURL, response) -> NSURL in
+            let fileManager = NSFileManager.defaultManager()
+            let directoryURLs = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+            if directoryURLs.count == 0 {
+                return temporaryURL
             }
+            
+            let pathComponent = response.suggestedFilename
+            let localURL: NSURL = directoryURLs[0].URLByAppendingPathComponent(pathComponent!)
+            
+            let video = Video()
+            video.title = URL.absoluteString
+            video.URLString = localURL.absoluteString
+            let realm = try! Realm()
+            try! realm.write {
+                realm.add(video)
+            }
+            
+            return localURL
+        }).progress { bytesRead, totalBytesRead, totalBytesExpectedToRead in
+            print("byte:\(bytesRead) total: \(totalBytesRead) totalExpect: \(totalBytesExpectedToRead)")
         }
+
     }
 }
