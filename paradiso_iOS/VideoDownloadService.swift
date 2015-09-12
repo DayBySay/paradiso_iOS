dc32d12cf1a135aee7321d7a42e92f0e22aff420
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-09-12 17:22:54 +0900

    ホワイトリストに該当する文字列を持っている場合リクエストを飛ばさないよう修正

--- paradiso_iOS/VideoDownloadService.swift
+++ paradiso_iOS/VideoDownloadService.swift
@@ -14,4 +14,22 @@ class VideoDownloadService {
     static func downloadWithURL(URL: NSURL, destination: (NSURL, NSHTTPURLResponse) -> NSURL) -> Request {
         return Alamofire.download(.GET, URL, destination: destination)
     }
+    
+    static func downloadableURL(URL: NSURL!) -> Bool {
+        return VideoDownloadService.isDownloadable(URL)
+    }
+    
+    static func isDownloadable(URL: NSURL) -> Bool {
+        for text in VideoDownloadService.whiteList() {
+            if URL.absoluteString.containsString(text) {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+     static private func whiteList() -> Array<String> {
+        return ["xvideos.com/videos/3gp"]
+    }
 }
