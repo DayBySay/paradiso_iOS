6fe77a6e15771806a165d2180ee2d2fdc695d15e
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-09-12 18:27:39 +0900

    ファイルが変なとこにいたので移動

---
+++ paradiso_iOS/VideoFeedsTableViewController.swift
@@ -0,0 +1,65 @@
+//
+//  VideoFeedsTableViewController.swift
+//  paradiso_iOS
+//
+//  Created by 清 貴幸 on 2015/09/12.
+//  Copyright © 2015年 Team Paradiso. All rights reserved.
+//
+
+import UIKit
+import SVWebViewController
+
+class VideoFeedsTableViewController: UITableViewController, UIWebViewDelegate {
+    let videofeeds: [String] = ["http://jp.xvideos.com/video4234045/an_asian_girl_is_sitting_on_a_bed_playing_with_her_from_http_alljapanese.net" ,"http://jp.xvideos.com/video14375503/siro-2465_3min", "http://jp.xvideos.com/video10236100/blowjob_1_15_2015"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func showVideoDownloadAlertController(downloadbleURL: NSURL!) {
+        let alertController = UIAlertController(title: "", message: "ダウンロード可能です", preferredStyle: .ActionSheet)
+        alertController.addAction(UIAlertAction(title: "ダウンロード", style: .Default, handler: { action in
+            VideoDownloadService.downloadableURL(downloadbleURL)
+        }))
+        
+        self.presentViewController(alertController, animated: true, completion: {})
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return videofeeds.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("VideoFeedsTableViewCell", forIndexPath: indexPath)
+        cell.textLabel?.text = videofeeds[indexPath.row]
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let webViewController = SVWebViewController(URL: NSURL(string: videofeeds[indexPath.row]))
+        webViewController.delegate = self
+        self.navigationController?.pushViewController(webViewController, animated: true)
+    }
+
+    
+    // MARK: - UIWebViewDelegate
+    
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        if VideoDownloadService.downloadableURL(request.URL) {
+            showVideoDownloadAlertController(request.URL)
+        }
+        return !VideoDownloadService.downloadableURL(request.URL)
+    }
+}
