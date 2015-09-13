2943399bdf41fce891b59aa0b134d206170905df
Takayuki Sei <takayuki_sei@voyagegroup.com>
2015-09-13 19:49:55 +0900

    サイズがおかしかったので修正

--- paradiso_iOS/DownloadProgressView.swift
+++ paradiso_iOS/DownloadProgressView.swift
@@ -17,6 +17,7 @@ class DownloadProgressView: UIView {
     
     static func showInView(view: UIView, completionHandler: DownloadCompletionHandler) {
         let progressView = UINib(nibName: "DownloadProgressView", bundle: nil).instantiateWithOwner(self, options: nil).first as! DownloadProgressView
+        progressView.frame = CGRectMake(0, CGRectGetHeight(view.frame) - CGRectGetHeight(progressView.frame), CGRectGetWidth(view.frame), CGRectGetHeight(progressView.frame))
         view.addSubview(progressView)
     }
 }
